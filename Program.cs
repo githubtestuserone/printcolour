adc53e160317a9a6c81c8fba4f5a46f3a73d2333
githubtestusertwo <githubusertwo@gmail.com>
2023-08-13 10:09:15 +1200

    Committing changes for printgreen

--- Program.cs
+++ Program.cs
@@ -3,3 +3,7 @@ Console.ForegroundColor = ConsoleColor.Blue;
 Console.WriteLine("I'm gonna print in Blueu");
 Console.ResetColor();
 
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("I'm gonna print in Green");
+Console.ResetColor();
+
